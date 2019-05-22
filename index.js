2984cb350efe1380520cfb11dcebd4bea16f478d
rapidstar <cse.juneo@gmail.com>
2019-05-22 16:27:35 +0900

    auth complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var mysql = require('mysql');
+var request = require('request');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -26,10 +27,36 @@ app.get('/join', function (req, res) {
     res.render('join')
   })
 
+app.get('/authResult', function (req, res) {
+    var auth_code = req.query.code;
+    var getTokenUrl = "https://testapi.open-platform.or.kr/oauth/2.0/token";
+    var option = {
+        method : "POST",
+        url :getTokenUrl,
+        headers : {
+        },
+        form : {
+            code : auth_code,
+            client_id : "l7xxcf48e8994cac43e1b329def19c111b6a",
+            client_secret : "42ad05c7506b46a29e93c3b37cfdcba4",
+            redirect_uri : "http://localhost:3000/authResult",
+            grant_type : "authorization_code"
+        }
+    };
+
+    request(option, function (err, response, body){
+        if(err) throw err;
+        else {
+            console.log(body);
+            var accessRequestResult = JSON.parse(body);
+            console.log(accessRequestResult);
+            res.render("resultChild", {data : accessRequestResult})
+        }
+    })
+})
+
 app.post('/join', function(req, res){
-
-    console.log(req.body);
-
+    console.log(req);
     var name = req.body.name;
     var birthday = new Date();
     var email = req.body.email;
@@ -37,7 +64,6 @@ app.post('/join', function(req, res){
     var phone = "01012345678";
 
     console.log(name, email, password);
-
     var sql = 'INSERT INTO `fintech`.`user` (`name`, `birthday`, `user_id`, `user_password`, `phone`) VALUES (?,?,?,?,?);'
     connection.query(sql, [name, birthday, email, password, phone], function (error, results) {
       if (error) throw error;
@@ -48,10 +74,12 @@ app.post('/join', function(req, res){
     });
 })
 
-
 app.get('/ajaxTest', function(req, res){
     console.log("ajax call");
     var result = "hello";
     res.json(result);
 })
+
+
+
 app.listen(3000)
